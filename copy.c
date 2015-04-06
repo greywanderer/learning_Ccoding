edfc84a95cd75b8482e8cca870c97c70e2cc1021
murthy <murthy.mellachervu@avagotech.com>
2015-04-06 12:18:54 -0500

    initial commit

---
+++ copy.c
@@ -0,0 +1,14 @@
+/* Copies contents of one file to another
+** Prompts user for name of source and destination
+** Use POSIX API
+** Include necessary error checking.
+** - check if source exists.
+** - check if destination has enough memory
+** Use "strace" to trace system calls
+*/
+#include <stdio.h>
+int main()
+{
+  printf("Hello World!\n");
+  return 0;
+}
